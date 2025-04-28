038261251c0a6ce6451f229f757e46eea385b0d3
daniellew77 <danielle_whisnant@brown.edu>
2025-04-28 00:16:44 -0400

    cnn only

--- only_cnn.py
+++ only_cnn.py
@@ -30,28 +30,6 @@ def spectogram_calc(input_shape):
     # spectogram_output = spectogram_model(spectogram_data)
     return spectogram_model
 
-def tabular_calc(input_shape):
-
-    # model to handle tabular data
-    tabular_model = tf.keras.Sequential([
-        tf.keras.layers.Input(shape=input_shape),
-        tf.keras.layers.Dense(64, activation='relu'),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Dropout(0.3),
-        tf.keras.layers.Dense(32, activation='relu')
-    ])
-    # tabular_output = tabular_model(tracks)
-    return tabular_model
-
-def final_calc(num_genres):
-    # final few layers to run concatenated outputs through, end w softmax
-    final_model = tf.keras.Sequential([
-        tf.keras.Dense(64, activation='relu'),
-        tf.keras.Dropout(0.3),
-        tf.keras.Dense(num_genres, activation='softmax')
-    ])
-    return final_model
-
 
 def build_full_model(spectogram_shape, tabular_shape, num_genres):
 
@@ -59,27 +37,13 @@ def build_full_model(spectogram_shape, tabular_shape, num_genres):
     # spectograms = utils.load("INSERT FILE PATH FROM PREPROCESSING")
     # tracks = utils.load("data/fma_metadata/tracks.csv")
 
-    # run both inputs through model
     spectogram_model = spectogram_calc(spectogram_shape)
-    # spectogram_output = spectogram_output(spectograms)
-    tabular_model = tabular_calc(tabular_shape)
 
     spectogram_input = tf.keras.Input(shape=spectogram_shape)
-    tabular_input = tf.keras.Input(shape=tabular_shape)
 
     spectogram_output = spectogram_model(spectogram_input)
-    tabular_output = tabular_model(tabular_input)
 
-    # concatenate both outputs
-    combined = tf.keras.layers.Concatenate()
-    output = combined([spectogram_output, tabular_output])
-
-    # find final output
-    final_model = final_calc(num_genres)
-    final_output = final_model(output)
-
-    model = tf.keras.Model(inputs=[spectogram_input, tabular_input], outputs=final_output)
-    return model
+    return spectogram_output
 
 
 
